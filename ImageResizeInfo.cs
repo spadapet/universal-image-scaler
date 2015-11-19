c34ca585a02e41fe156422e7c3d690a0721ca035
Peter Spada <spadapet@hotmail.com>
2015-11-18 20:35:03 -0800

    ManifestImageInfo

--- ImageResizeInfo.cs
+++ ImageResizeInfo.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using Microsoft.VisualStudio.Shell.Interop;
 
 namespace UniversalImageScaler
 {
-    public class ImageResizeInfo
+    internal class ImageResizeInfo // : INotifyPropertyChanged
     {
         private VSITEMSELECTION item;
         private string path;
@@ -46,6 +47,21 @@ namespace UniversalImageScaler
             get
             {
                 return Path.GetFileName(this.path);
+            }
+        }
+
+        public IEnumerable<ManifestImageInfo> ManifestImages
+        {
+            get
+            {
+                yield return new ManifestImageInfo(71, 71);
+                yield return new ManifestImageInfo(150, 150);
+                yield return new ManifestImageInfo(310, 150);
+                yield return new ManifestImageInfo(310, 310);
+                yield return new ManifestImageInfo(44, 44, 256, 48, 24, 16);
+                yield return new ManifestImageInfo(50, 50);
+                yield return new ManifestImageInfo(24, 24);
+                yield return new ManifestImageInfo(620, 300);
             }
         }
     }
---
+++ ManifestImageInfo.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalImageScaler
+{
+    internal class ManifestImageInfo
+    {
+        private int width;
+        private int height;
+        private int[] targetSizes;
+
+        public ManifestImageInfo(int width, int height, params int[] targetSizes)
+        {
+            this.width = width;
+            this.height = height;
+            this.targetSizes = targetSizes ?? new int[0];
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+        }
+
+        public IEnumerable<double> Scales
+        {
+            get
+            {
+                yield return 1.0;
+                yield return 1.25;
+                yield return 1.5;
+                yield return 2.0;
+                yield return 4.0;
+            }
+        }
+
+        public IEnumerable<int> TargetSizes
+        {
+            get
+            {
+                return this.targetSizes;
+            }
+        }
+
+        public int GetScaledWidth(double scale)
+        {
+            return (int)(this.width * scale);
+        }
+
+        public int GetScaledHeight(double scale)
+        {
+            return (int)(this.height * scale);
+        }
+
+        public bool IsSquare
+        {
+            get
+            {
+                return this.width == this.height;
+            }
+        }
+
+        public bool IsWide
+        {
+            get
+            {
+                return this.width != this.height;
+            }
+        }
+    }
+}
