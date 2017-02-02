878a6bfc13401c8d55b099c795660e9ebeda0867
Peter Spada <peterspa@microsoft.com>
2017-02-02 10:30:27 -0800

    Add support for VS 2017 RC

--- ImageResizePackage.cs
+++ ImageResizePackage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -18,27 +17,16 @@ namespace UniversalImageScaler
         public const string PackageGuidString = "9bd56ba2-ecfa-4ff8-8615-0475808ff596";
         private const string CommandSetGuidString = "d4e44266-2d61-4268-ac51-b3392512cbbf"; 
         private const int ImageResizeCommandId = 1;
-#if DEBUG
-        private const string AppInsightsGuidString = "b8bb3d3c-a649-4f10-87f2-dabac88e877f";
-#else
-        private const string AppInsightsGuidString = "4860a93a-8a6c-4af5-aa85-f744c147884e";
-#endif
         public static ImageResizePackage Instance { get; private set; }
         public ThreadHelper MainThreadHelper { get; }
 
         private List<MenuCommand> menuCommands;
         private IMenuCommandService menuCommandService;
-        private Microsoft.ApplicationInsights.TelemetryClient telemetryClient;
 
         public ImageResizePackage()
         {
             this.MainThreadHelper = ThreadHelper.Generic;
             this.menuCommands = new List<MenuCommand>();
-        }
-
-        public Microsoft.ApplicationInsights.TelemetryClient TelemetryClient
-        {
-            get { return this.telemetryClient; }
         }
 
         protected override void Initialize()
@@ -48,17 +36,8 @@ namespace UniversalImageScaler
                 ImageResizePackage.Instance = this;
             }
 
-            InitializeTelemetry();
             base.Initialize();
             AddMenuCommands();
-        }
-
-        private void InitializeTelemetry()
-        {
-            this.telemetryClient = new Microsoft.ApplicationInsights.TelemetryClient()
-            {
-                InstrumentationKey = ImageResizePackage.AppInsightsGuidString,
-            };
         }
 
         protected override void Dispose(bool disposing)
